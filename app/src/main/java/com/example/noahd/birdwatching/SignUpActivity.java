a3cb03025f724d0a824aedae5ce864c4d34ab40b
Noahtyr <Noahdavidian@hotmail.dk>
2018-03-22 13:12:24 +0100

    You can now register people on the database and they are saved! Will use these to check logins in MainActivity

--- app/src/main/java/com/example/noahd/birdwatching/SignUpActivity.java
+++ app/src/main/java/com/example/noahd/birdwatching/SignUpActivity.java
@@ -1,13 +1,19 @@
 package com.example.noahd.birdwatching;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -15,17 +21,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
     EditText SignUpUN, SignUpPW;
     private FirebaseAuth mAuth;
-
-
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-       // updateUI(currentUser);
-    }
-
+    ProgressBar createProgress;
 
 
     @Override
@@ -39,6 +35,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         SignUpUN = (EditText) findViewById(R.id.SignUpUN);
         SignUpPW = (EditText) findViewById(R.id.SignUpPW);
         mAuth = FirebaseAuth.getInstance();
+        createProgress = (ProgressBar) findViewById(R.id.createProgress);
     }
 
 
@@ -78,6 +75,21 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
             SignUpPW.requestFocus();
             return;
         }
+
+        createProgress.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                    createProgress.setVisibility(View.GONE);
+                    Toast.makeText(getApplicationContext(),"User Reqistered!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed to register", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 
 
