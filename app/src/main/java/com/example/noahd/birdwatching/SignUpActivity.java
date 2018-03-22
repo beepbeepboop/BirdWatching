ec9fba6f6757b69bc546efb3a6f573b19baec83f
Noahtyr <Noahdavidian@hotmail.dk>
2018-03-22 10:44:34 +0100

    Added small changes and pictures of Birbs

--- app/src/main/java/com/example/noahd/birdwatching/SignUpActivity.java
+++ app/src/main/java/com/example/noahd/birdwatching/SignUpActivity.java
@@ -2,12 +2,29 @@ package com.example.noahd.birdwatching;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTextUserName, EditTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()) {
+            case R.id.CreateUser:
+
+                break;
+
+
+        }
+
     }
 }
