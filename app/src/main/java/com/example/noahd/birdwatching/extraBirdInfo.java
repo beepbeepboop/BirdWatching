8adb01da477a9e5c3383739781d4d21175dd84c6
Juaquin <Noahdavidian@hotmail.dk>
2018-04-24 17:55:42 +0200

    added some stuff
    added some more stuff from previous activities to preserve continuity

--- app/src/main/java/com/example/noahd/birdwatching/extraBirdInfo.java
+++ app/src/main/java/com/example/noahd/birdwatching/extraBirdInfo.java
@@ -3,6 +3,8 @@ package com.example.noahd.birdwatching;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,6 +13,9 @@ import com.example.noahd.birdwatching.R;
 
 public class extraBirdInfo extends AppCompatActivity {
 
+    DrawerLayout mDrawerlayout;
+    private ActionBarDrawerToggle mToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +23,18 @@ public class extraBirdInfo extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerlayout,R.string.open, R.string.close);
+        mDrawerlayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        if(toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle("");
+
+        }
+
+
     }
 
 }
