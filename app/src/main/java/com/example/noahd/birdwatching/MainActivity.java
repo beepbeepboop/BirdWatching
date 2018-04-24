46c51a26283d77fa82d50f0fd19ada94f8c2f894
Juaquin <Noahdavidian@hotmail.dk>
2018-04-24 21:24:30 +0200

    added more
    added some more stuff, used Simon as help! adding code to show pictures! yas

--- app/src/main/java/com/example/noahd/birdwatching/MainActivity.java
+++ app/src/main/java/com/example/noahd/birdwatching/MainActivity.java
@@ -43,9 +43,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         SignUpUN = (EditText) findViewById(R.id.SignUpUN);
         SignUpPW = (EditText) findViewById(R.id.SignUpPW);
         createProgress = (ProgressBar) findViewById(R.id.createProgress);
-
-
-
         frontpagePic = (ImageView) findViewById(R.id.frontpagePic);
 
         int imageResource = getResources().getIdentifier("@drawable/birbo", null, this.getPackageName());
--- app/src/main/java/com/example/noahd/birdwatching/extraBirdInfo.java
+++ app/src/main/java/com/example/noahd/birdwatching/extraBirdInfo.java
@@ -1,5 +1,6 @@
 package com.example.noahd.birdwatching;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +9,22 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
 
+import com.bumptech.glide.Glide;
 import com.example.noahd.birdwatching.R;
 
-public class extraBirdInfo extends AppCompatActivity {
+import java.util.HashMap;
+
+public class extraBirdInfo extends AppCompatActivity  {
 
     DrawerLayout mDrawerlayout;
     private ActionBarDrawerToggle mToggle;
+    private Button createBtn;
+    String Url ="";
+    private HashMap<String, String> birdInfoHashMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +33,20 @@ public class extraBirdInfo extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ImageView imageView = (ImageView) findViewById(R.id.birdImageView);
+        createBtn = findViewById(R.id.createButton);
+        Intent intent = getIntent();
+        createBtn.setOnClickListener(buttonListener);
+        birdInfoHashMap = (HashMap<String, String>) intent.getSerializableExtra("bird");
+        Url = birdInfoHashMap.get("PhotoUrl");
+
         mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer);
         mToggle = new ActionBarDrawerToggle(this,mDrawerlayout,R.string.open, R.string.close);
         mDrawerlayout.addDrawerListener(mToggle);
         mToggle.syncState();
+
+
+        Glide.with(this).load(Url).into(imageView);
 
         if(toolbar != null) {
             setSupportActionBar(toolbar);
@@ -36,5 +56,19 @@ public class extraBirdInfo extends AppCompatActivity {
 
 
     }
+
+    private View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            switch (view.getId()){
+                
+
+            }
+
+        }
+    }
+
+
 
 }
