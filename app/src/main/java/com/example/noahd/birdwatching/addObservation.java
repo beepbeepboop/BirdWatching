de191d85a25b61c3ac7c8545b453b6c442e62662
Juaquin <Noahdavidian@hotmail.dk>
2018-04-25 20:54:27 +0200

    Added Camera functionality!
    Added Camera functionalty to the App, you can now take pictures of the bird! all is shown in the add observation Activity!

--- app/src/main/java/com/example/noahd/birdwatching/addObservation.java
+++ app/src/main/java/com/example/noahd/birdwatching/addObservation.java
@@ -1,7 +1,9 @@
 package com.example.noahd.birdwatching;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class addObservation extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,6 +20,8 @@ public class addObservation extends AppCompatActivity implements View.OnClickLis
     EditText times;
     EditText longtitude, latitude;
     Button createObser;
+    ImageView cameraImage;
+    ImageButton addImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +30,43 @@ public class addObservation extends AppCompatActivity implements View.OnClickLis
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        // Handles Specifics
         species = (EditText) findViewById(R.id.speciesEdit);
         times = (EditText) findViewById(R.id.timeEdit);
         longtitude = (EditText) findViewById(R.id.longtitudeEdit);
         latitude = (EditText) findViewById(R.id.latitudeEdit);
 
+         // Handles Camera Function
+        cameraImage = (ImageView) findViewById(R.id.cameraView);
+        addImage = (ImageButton) findViewById(R.id.addImageBtn);
+
+
+        // Handles the creation
         createObser = (Button) findViewById(R.id.createOb);
+
+
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+
+
+            }
+        });
 
 
 
         }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        cameraImage.setImageBitmap(bitmap);
+    }
 
     @Override
     public void onClick(View view) {
--- app/src/main/java/com/example/noahd/birdwatching/yourObservations.java
+++ app/src/main/java/com/example/noahd/birdwatching/yourObservations.java
@@ -22,15 +22,6 @@ public class yourObservations extends AppCompatActivity {
         setContentView(R.layout.activity_your_observations);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         recieve = (TextView) findViewById(R.id.recieve);
 
 
